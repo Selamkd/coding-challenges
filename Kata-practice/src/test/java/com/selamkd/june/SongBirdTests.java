65d5b791b1670435f9c9ec120e326f56083466b6
“selam” <Selamkd@users.noreply.github.com>
2024-06-28 00:21:30 +0100

    songBird add first test

--- Kata-practice/src/test/java/com/selamkd/june/SongBirdTests.java
+++ Kata-practice/src/test/java/com/selamkd/june/SongBirdTests.java
@@ -1,8 +1,22 @@
 package com.selamkd.june;
 
-import javax.annotation.processing.SupportedAnnotationTypes;
+import com.selamkd.june.songbird.SongBird;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.Assert.assertEquals;
 
 public class SongBirdTests {
 
+    @Test
+    @DisplayName("Check that it returns 0 when there are no birds")
+
+    public void checkItReturns0whenGiven0Birds(){
+
+
+
+
+    }
 
 }
