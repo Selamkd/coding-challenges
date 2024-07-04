5d4bd7c5c6e95a31f22993b274bdd259bdfb8b79
“selam” <Selamkd@users.noreply.github.com>
2024-07-04 18:43:14 +0100

    time formatter Iter2

--- Kata-practice/src/main/java/com/selamkd/July/TimeFormatter/TimeFormatter.java
+++ Kata-practice/src/main/java/com/selamkd/July/TimeFormatter/TimeFormatter.java
@@ -1,30 +1,54 @@
 package com.selamkd.July.TimeFormatter;
 
 public class TimeFormatter {
+    public static void main(String[] args) {
 
+        System.out.println(formatDuration(120));
+    }
     public static String formatDuration(int seconds) {
+
+        if(seconds < 60){
+            return seconds + " second";
+        }
+        if(seconds == 3600){
+            return "1 hour";
+        }
         StringBuilder result  = new StringBuilder();
+
+        if(seconds > 31536000 ){
+            int years = (int) (seconds/ 31536000);
+            seconds = seconds -  31536000;
+            result.append(years).append( years == 1 ?  "year," : " years ");
+        }
+
+        if(seconds > 86400){
+            int days = seconds/ 86400;
+            seconds = seconds - 86400;
+            result.append(days).append( days == 1 ? " day, " : " days, ");
+        }
+
+        if(seconds >= 3600 ){
+            int hours = seconds / 3600;
+            seconds = seconds - 3600;
+            result.append(hours).append( hours == 1 ? " hour, " : "hours, ");
+        }
+
         if(seconds > 60) {
             int minutes = seconds / 60;
-            result.append(minutes).append(" minutes ");
+            seconds = seconds - minutes * 60;
+            result.append(minutes).append( minutes == 1 ? " minute " : " minutes");
         }
-        if(seconds > 3600 ){
-            int hours = seconds / 3600;
-            result.append(hours).append(" hours ");
-        }
-      if(seconds > 86400){
-          int days = seconds/ 86400;
-          result.append(days).append(" days ");
-      }
-      if(seconds > 3.1556952){
-          int years = (int) (seconds/3.1556952);
-          result.append(years).append(" year");
-      }
 
 
-      result.append(seconds).append(" seconds.");
 
 
+
+
+          if(seconds >= 1) {
+              result.append("and ");
+              result.append(seconds).append(" seconds");
+
+          }
         return result.toString();
     }
 }
