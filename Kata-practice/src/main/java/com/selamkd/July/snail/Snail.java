b490e093f586c3b4921c3663ef1c5ffcc2f1f751
“selam” <Selamkd@users.noreply.github.com>
2024-07-09 21:23:57 +0100

    Snail Iter 2 for 4 * 4 matrix

--- Kata-practice/src/main/java/com/selamkd/July/snail/Snail.java
+++ Kata-practice/src/main/java/com/selamkd/July/snail/Snail.java
@@ -6,16 +6,23 @@ public class Snail {
 
     public static int[] snail(int[][] array) {
         ArrayList<Integer> result = new ArrayList<>();
+        // 1239
 
-        result.add(array[0][0]);
-        result.add(array[0][1]);
-        result.add(array[0][2]);
-        result.add(array[1][2]);
-        result.add(array[2][2]);
-        result.add(array[2][1]);
-        result.add(array[2][0]);
-        result.add(array[1][0]);
-        result.add(array[1][1]);
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[0][i]);
+        }
+
+
+        for(int i = 0; i < array.length; i++){
+         System.out.println(array[i][array.length - 1]);
+
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            // reverse before adding
+            System.out.println(array[array.length - 1][i]);
+        }
+
 
         return result.stream().mapToInt(i -> i ).toArray();
     }
--- Kata-practice/src/test/java/com/selamkd/July/SnailTests.java
+++ Kata-practice/src/test/java/com/selamkd/July/SnailTests.java
@@ -1,4 +1,4 @@
-package com.selamkd.June;
+package com.selamkd.July;
 
 import com.selamkd.July.snail.Snail;
 import org.junit.jupiter.api.Assertions;
@@ -16,6 +16,21 @@ public class SnailTests {
               {7, 8, 9}};
       int[] expectedOutput = {1, 2, 3, 6, 9, 8, 7, 4, 5};
       int[] actualOutput = Snail.snail(TDArrayInput);
+
+        Assertions.assertArrayEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    @DisplayName("Given a 2D array should elements from outermost to middle- 4 elements per row")
+    void givenA2DArrayShouldElementsFromOutermostToMiddleFourElementsRow() {
+        int[][] TDArrayInput = {{1, 2, 3,9},
+                {4, 5, 6, 4},
+                {7, 8, 9,1 },
+                {1, 2, 3, 4 },
+
+        };
+        int[] expectedOutput = {1, 2, 3, 9, 4, 1, 4, 3, 2, 1, 7, 4, 5, 6, 9, 8};
+        int[] actualOutput = Snail.snail(TDArrayInput);
 
         Assertions.assertArrayEquals(expectedOutput, actualOutput);
     }
