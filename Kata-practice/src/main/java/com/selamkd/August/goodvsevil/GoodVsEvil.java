67b71eda025c708cf2160e8747661f30309494f2
“selam” <Selamkd@users.noreply.github.com>
2024-08-14 22:09:07 +0100

    good vs evil iter 1

---
+++ Kata-practice/src/main/java/com/selamkd/August/goodvsevil/GoodVsEvil.java
@@ -0,0 +1,23 @@
+package com.selamkd.August.goodvsevil;
+
+import java.util.Map;
+
+public class GoodVsEvil {
+    public static void main(String[] args) {
+
+    }
+
+
+    public static String battle(String goodAmounts, String evilAmounts) {
+        Map<String, Integer> forceGood = Map.of("Orcs", 1,
+                "Men", 2, "Wargs", 2, "Goblins", 2, "Uruk Hai", 3, "Trolls", 5, "Wizards",10);
+
+        Map<String, Integer> forceEvil = Map.of("Hobbits", 1,
+                "Men", 2, "Elves", 3, "Dwarves", 3, "Eagles", 4, "Wizards", 10);
+
+
+
+
+        return "";
+    }
+}
