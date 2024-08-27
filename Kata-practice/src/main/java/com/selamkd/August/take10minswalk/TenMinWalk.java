b9cdc9efcffb4b0b5bf3af734827e17d6778ef85
“selam” <Selamkd@users.noreply.github.com>
2024-08-27 14:36:28 +0100

    add new kata - take a ten minute walk

---
+++ Kata-practice/src/main/java/com/selamkd/August/take10minswalk/TenMinWalk.java
@@ -0,0 +1,7 @@
+package com.selamkd.August.take10minswalk;
+
+public class TenMinWalk {
+    public static boolean isValid(char[] walk) {
+        return true;
+    }
+}
---
+++ Kata-practice/src/test/java/com/selamkd/August/TenMinWalkTests.java
@@ -0,0 +1,15 @@
+package com.selamkd.August;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TenMinWalkTests {
+    
+    @Test
+    @DisplayName("Should return true given 'n','s','n','s','n','s','n','s','n','s'")
+    void shouldReturnTrueGivenNSNSNSNSNS() {
+
+        
+        
+    }
+}
