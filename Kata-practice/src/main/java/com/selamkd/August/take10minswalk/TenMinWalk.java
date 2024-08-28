f3f80c61f675cf06fcf976a28ec54fc4d25c2f10
“selam” <Selamkd@users.noreply.github.com>
2024-08-28 14:26:20 +0100

    10 min walk iter 1

--- Kata-practice/src/main/java/com/selamkd/August/take10minswalk/TenMinWalk.java
+++ Kata-practice/src/main/java/com/selamkd/August/take10minswalk/TenMinWalk.java
@@ -2,6 +2,28 @@ package com.selamkd.August.take10minswalk;
 
 public class TenMinWalk {
     public static boolean isValid(char[] walk) {
-        return true;
+
+        if(walk.length == 10) return true;
+        int n =0;
+        int s = 0;
+        int e = 0;
+        int w = 0;
+
+        for(char ch : walk){
+            switch(ch){
+                case 'n':
+                    n+=1;
+                case 's':
+                    s+=1;
+                case 'w':
+                    w+=1;
+                case 'e':
+                    e+=1;
+
+            }
+        }
+
+
+        return (n == 0 && s == 0 && e == 0 && w == 0 );
     }
 }
