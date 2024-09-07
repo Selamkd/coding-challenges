7de450884bcee37d5442abb1f48402a248403364
Selam 🤍 <selamkedir@icloud.com>
2024-09-07 14:41:05 +0100

    Delete Kata-practice/TypeScript directory

--- Kata-practice/TypeScript/addLength /add-length.js
+++
@@ -1,9 +0,0 @@
-export function addLength(str) {
-    // your code here
-    let result = [];
-    let word = str.split(' ');
-    for (let i = 0; i < word.length; i++) {
-        result.push(word[i] + " " + word[i].length);
-    }
-    return result;
-}
