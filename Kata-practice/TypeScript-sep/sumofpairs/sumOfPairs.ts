17daeb783ebaedfac0f569340702e9a4ff977bd4
“selam” <Selamkd@users.noreply.github.com>
2024-09-13 21:28:01 +0100

    add iter 1 solution - sumOfPairs

--- Kata-practice/TypeScript-sep/sumofpairs/sumOfPairs.ts
+++ Kata-practice/TypeScript-sep/sumofpairs/sumOfPairs.ts
@@ -1,3 +1,22 @@
 export function sumPairs(ints: number[], s: number): [number, number] | void {
-    return undefined // your code here...
+   const possiblePairs: number[][] = getPossiblePairs(ints);
+   for(const pair of possiblePairs) {
+       const sum = pair[0] + pair[1];
+       if(sum === s){
+          return [pair[0], pair[1]];
+       }
+   }
+    return undefined;
+}
+
+
+sumPairs([1,2,3,5,5,7,8],8)
+function getPossiblePairs(numArr: number[]){
+    const result:number[][] = [];
+    for(let i= 0; i < numArr.length; i++){
+        for(let j = i; j < numArr.length -1; j++){
+            result.push([numArr[i], numArr[j + 1]]);
+        }
+    }
+    return result;
 }
