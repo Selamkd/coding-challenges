a126c2f82303fec827f781826739aa16166efc66
“selam” <Selamkd@users.noreply.github.com>
2024-09-13 21:57:29 +0100

    spin words - add solution

--- Kata-practice/TypeScript-sep/spin-words/spinWords.ts
+++ Kata-practice/TypeScript-sep/spin-words/spinWords.ts
@@ -1,3 +1,21 @@
+import logger from "../utils/logger.ts"
+
 export function spinWords(words: string): string {
-return "Hello there!"
+    const wordArray = words.split(" ");
+    let result = "";
+    for (let word of wordArray) {
+        logger.info("Entering loop ...." + word);
+        if (word.length >= 5) {
+            logger.info("greater than 5 chars ->" + word);
+            let reversedWord = word.split('').reverse().join('')
+            result += reversedWord + " ";
+            logger.info("Reversed word -> " + reversedWord);
+        } else {
+            logger.info("less than 5 chars  ->" + word);
+            result += word + " ";
+        }
+    }
+    return result.trim();
 }
+
+spinWords("This is another test");
