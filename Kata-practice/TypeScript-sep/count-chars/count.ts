6a28a4e8c132577cec40a68a4f720e5fa24dd2f6
“selam” <Selamkd@users.noreply.github.com>
2024-09-21 22:10:07 +0100

    add solution to count chars

--- Kata-practice/TypeScript-sep/count-chars/count.ts
+++ Kata-practice/TypeScript-sep/count-chars/count.ts
@@ -1,6 +1,18 @@
+
+
 function count(str: string): {[key:string] : number}{
+    let countMap: Map<string, number> = new Map();
+    if(!str){
+        return {};
+    }
+    for(let char of str){
+        countMap.set(char, getOrDefault(countMap,char, 0 ) + 1)
+        console.log(countMap.get(char));
+    }
+    return Object.fromEntries(countMap);
+}
 
-
-
-    return {}
+console.log(count("ab"));
+function getOrDefault<K,V> (map: Map<K, V>, key: K, defaultValue:V): V {
+    return map.has(key) ? map.get(key)! : defaultValue;
 }
