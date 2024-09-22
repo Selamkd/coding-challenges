3953fb59c86bd7e4ef5ccf10fd299d724a991038
“selam” <Selamkd@users.noreply.github.com>
2024-09-22 08:53:46 +0100

    domain name - add final solution

--- Kata-practice/TypeScript-sep/extract-domain/domainName.ts
+++ Kata-practice/TypeScript-sep/extract-domain/domainName.ts
@@ -1,9 +1,15 @@
 function domainName(url:string):string{
-    //your code here
-    // All names come before a dot
-    // some of them are prifexed by d
-
-    return url.substring(url.indexOf("/") + 2, url.indexOf("."));
+   /*
+   * (/.+\/\/|www.|\..+/g, '') matches:
+   * (/.+\/\/)- a protocol prefix like http:// or https://
+   *  (www) - part of the url
+   * (\..+) - domain extension like .com, .org..etc
+   * */
+    return url.replace(/.+\/\/|www.|\..+/g, '');
 }
 
 console.log(domainName("http://google.com"));
+console.log(domainName("http://google.co.jp"));
+console.log(domainName("www.xakep.ru"), "xakep");
+console.log(domainName("http://www.zombie-bites.com"));
+console.log(domainName("https://www.cnet.com"));
