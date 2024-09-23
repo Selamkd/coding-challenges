807360a2ae4c9fd037e74a2c7f0dc1cb1fb64848
“selam” <Selamkd@users.noreply.github.com>
2024-09-23 22:25:26 +0100

    to camel case - add solution

--- Kata-practice/TypeScript-sep/toCamelCase/toCamelCase.ts
+++ Kata-practice/TypeScript-sep/toCamelCase/toCamelCase.ts
@@ -1,11 +1,11 @@
 function toCamelCase(str:string):string{
-    //convert dash into camel casing
-    //first word should only be capitilized if the original word was capitalized
-    // the next word should  always be capitilized
-    const newStr= str.replace(/_/g, '').split(',')
-    const result = newStr.map(word=>{
-        const capitilizedWord = word.charAt(0).toUpperCase()+ word.slice(1)
-        console.log(capitilizedWord)
-    })
-   return result.join('');
+
+    const camelCaseStr = str.split('_').map((word, index) => {
+        return index === 0 ? word : word.charAt(0).toUpperCase() + word.slice(1);
+    }).join('');
+
+
+   return camelCaseStr;
 }
+
+console.log(toCamelCase("the_stealth_warrior"));
