23b1fcd9fe8a7d37c8cd0a2e7c2f3ecaa0125843
“selam” <Selamkd@users.noreply.github.com>
2025-04-18 22:39:37 +0100

    clear comments

--- Kata-practice/30-days-of-JS/helloWorld.js
+++ Kata-practice/30-days-of-JS/helloWorld.js
@@ -1,13 +1,9 @@
 var createHelloWorld = function () {
   return function (...args) {
-    // clear all arguments
     args.length = 0;
-
-    // add hello world str
     args.push('Hello');
     args.push('World');
 
-    // convert and return back as a string
     return args.join(' ');
   };
 };
